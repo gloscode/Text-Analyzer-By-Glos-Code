e1b8f4896043768a5d1b19db18f3bc8e99aeb4e6
Aarav <agupta5046@gmail.com>
2022-11-25 20:09:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -17,10 +17,6 @@ let home = document.querySelector(".home");
 let about = document.querySelector(".about");
 let body = document.querySelector(".body");
 
-// search.addEventListener("click", (e) => {
-//   e.preventDefault();
-// });
-
 navHeading.addEventListener("click", (e) => {
   e.preventDefault();
 });
